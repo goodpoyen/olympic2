42d99c49733a4885a4f3932fb3805d0d925c927e
poyen <goodpoyen@gamil.com>
2026-08-07 11:33:47 +0800

    fix login fail lock

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -307,6 +307,10 @@ export default {
               this.tokenService.store("Lcount", this.loginCount + 1, "900000");
             }
             this.makeCode();
+          } else if (response.data.code === 218) {
+            this.alertShow = true;
+            this.errorMsg = "登入失敗多次，請稍後再登入";
+            this.verifycode = "";
           } else {
             this.globalSystemTool.removeLocalStorage();
           }
