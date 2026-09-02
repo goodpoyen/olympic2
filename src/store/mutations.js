2b6e47ece21c4a162b9d12f17d54090db4d79cfa
poyen <goodpoyen@gamil.com>
2026-09-02 10:30:54 +0800

    add exame area date warning

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,41 +1,46 @@
-import * as types from './mutations_type.js'
+import * as types from "./mutations_type.js";
 // import Vue from 'vue'
 
 export const state = {
   count: 0,
-  title: '',
-  personnlFilter: '',
-  signupName: '',
-  signupStartTime: '',
-  signupEndTime: ''
-}
+  title: "",
+  areaExist: "",
+  personnlFilter: "",
+  signupName: "",
+  signupStartTime: "",
+  signupEndTime: "",
+};
 
 // mutations
 export const mutations = {
-  [types.INCREASE] (state) {
-    state.count += 1
+  [types.INCREASE](state) {
+    state.count += 1;
   },
-  [types.DECREASE] (state) {
-    state.count -= 1
-  },
-
-  [types.title] (state, status) {
-    state.title = status
+  [types.DECREASE](state) {
+    state.count -= 1;
   },
 
-  [types.personnlFilter] (state, status) {
-    state.personnlFilter = status
+  [types.title](state, status) {
+    state.title = status;
   },
 
-  [types.signupStartTime] (state, status) {
-    state.signupStartTime = status
+  [types.areaExist](state, status) {
+    state.areaExist = status;
   },
 
-  [types.signupEndTime] (state, status) {
-    state.signupEndTime = status
+  [types.personnlFilter](state, status) {
+    state.personnlFilter = status;
   },
 
-  [types.examCodeStatus] (state, status) {
-    state.examCodeStatus = status
-  }
-}
+  [types.signupStartTime](state, status) {
+    state.signupStartTime = status;
+  },
+
+  [types.signupEndTime](state, status) {
+    state.signupEndTime = status;
+  },
+
+  [types.examCodeStatus](state, status) {
+    state.examCodeStatus = status;
+  },
+};
--- src/store/mutations_type.js
+++ src/store/mutations_type.js
@@ -1,7 +1,8 @@
-export const INCREASE = 'INCREASE'
-export const DECREASE = 'DECREASE'
-export const title = 'title'
-export const personnlFilter = 'personnlFilter'
-export const signupStartTime = 'signupStartTime'
-export const signupEndTime = 'signupEndTime'
-export const examCodeStatus = 'examCodeStatus'
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+export const title = "title";
+export const areaExist = "areaExist";
+export const personnlFilter = "personnlFilter";
+export const signupStartTime = "signupStartTime";
+export const signupEndTime = "signupEndTime";
+export const examCodeStatus = "examCodeStatus";
